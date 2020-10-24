26d9bcffce100f8933394d94ea52258ef30d89ae
Davi Monteiro <davi.monteiro7@gmail.com>
2020-10-24 16:29:31 -0300

    feat: init backend server, inital archtecture, use ejs template engine and express.

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use('/public',express.static('./views/public'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+const fakeRepos = {
+    hasData: true,
+    name: 'fakename',
+    description: 'fakedescription',
+    fakedate: 'fakedate'
+}
+
+
+app.get('/', (req, res) => {
+    res.render('index', {fakeRepos});
+});
+
+
+
+app.listen(4000, () => console.log('Example app listening on port 4000'));
